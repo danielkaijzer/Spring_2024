9cc4087d1cdd8185f6c1f1bf418881a0ce5fac7a
danielkaijzer <danielkaijzer00@gmail.com>
2024-01-29 20:29:55 -0500

    Setup for LC110

---
+++ LC110_Balanced_Binary_Tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return false;
+    }
+};
+
+// Driver
+int main(){
+    Solution s;
+
+    // Task 1: Construct a tree for testing
+    TreeNode * root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+
+    cout << s.isBalanced(root) << endl;
+
+    return 0;
+}
