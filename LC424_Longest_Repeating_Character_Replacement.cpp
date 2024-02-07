4214f79ec29a53916e8dfcaae68e1e64d11963aa
danielkaijzer <danielkaijzer00@gmail.com>
2024-02-06 23:15:25 -0500

    Solved LC424 but without doing much on my own. Need to try again a few times without looking at other people's code or explanations.

--- LC424_Longest_Repeating_Character_Replacement.cpp
+++ LC424_Longest_Repeating_Character_Replacement.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        
+
+        unordered_map<char,int> mp;
+
+        int l =0, r=0, maxval = 0, ans = -1;
+
+        for (;r < s.size(); ++r){
+            mp[s[r]]++; // iterate char in hashmap
+            maxval = std::max(maxval,mp[s[r]]); // update the most common value in string (so far)
+            ans = std::max(ans,r-l+1);
+
+            // check num of replacements needed
+            // length of window - count of most frequent character
+            while((r-l+1)-maxval > k){
+                mp[s[l]]--; // before we move pointer, decriment char count at left
+                l++; // move left pointer
+            }
+        }
+        return ans;
     }
 };
 
