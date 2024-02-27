7408045a54658f4de59969993a17dd33ca9a73af
danielkaijzer <danielkaijzer00@gmail.com>
2024-02-26 22:16:13 -0500

    Tried to remember the solution from memory.

--- LC238_Product_of_Array_Except_Self.cpp
+++ LC238_Product_of_Array_Except_Self.cpp
@@ -11,22 +11,20 @@ public:
 
         // prefix loop
         int prefix = 1;
+
         for (int i = 0; i < nums.size(); ++i){
             answer[i] = prefix;
-            prefix *= nums[i]; // this staggers so cur index will only have product of what comes before it
+            prefix *= nums[i];
         }
-        // res: [1,1,2,6]
 
-        // postfix loop
+        // prefix loop
         int postfix = 1;
-        for (int i = nums.size()-1; i >=0; --i){
-            answer[i] *= postfix;
-            postfix *= nums[i]; // again staggers so cur index will have product of what comes after it 
+
+        for (int i = nums.size(); i >=0; ++i){
+            answer[i] = postfix;
+            postfix *= nums[i];
         }
-        // res: [ (12*2) , (4*3) , (2*4) , (6*1)]
-
-
-        return answer;
+       
     }
 };
 
@@ -44,3 +42,27 @@ int main(){
 
 
 }
+
+
+
+/*
+ vector<int> answer(nums.size());
+
+        // prefix loop
+        int prefix = 1;
+        for (int i = 0; i < nums.size(); ++i){
+            answer[i] = prefix;
+            prefix *= nums[i]; // this staggers so cur index will only have product of what comes before it
+        }
+        // res: [1,1,2,6]
+
+        // postfix loop
+        int postfix = 1;
+        for (int i = nums.size()-1; i >=0; --i){
+            answer[i] *= postfix;
+            postfix *= nums[i]; // again staggers so cur index will have product of what comes after it 
+        }
+        // res: [ (12*2) , (4*3) , (2*4) , (6*1)]
+
+
+        return answer;*/
