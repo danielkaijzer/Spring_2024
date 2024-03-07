87987687adf6af819f6807660bca88f3c5362155
danielkaijzer <danielkaijzer00@gmail.com>
2024-03-06 23:51:33 -0500

    Starting an app to examine Premier League Player Stats. Modifying the NBA Player stats app made by Data Professor

---
+++ Streamlit_Apps/app_3_eda_basketball/eda_basketball.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+st.title('Premier League Player Stats Explorer')
