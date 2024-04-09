609cae5bc2ed847316284a916d885aa367862813
danielkaijzer <danielkaijzer00@gmail.com>
2024-04-09 16:16:46 -0400

    This solution works but exceeds the time limit on some test cases

--- LC560_Subarray_Sum_Equals_K.cpp
+++ LC560_Subarray_Sum_Equals_K.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <unordered_map>
 
@@ -8,35 +9,39 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0;
-
-
+        
+        // std::sort(nums.begin(), nums.end());
         // first make sure nums are sorted, for now ASSUME SORTED
-
         // check subarrays up to where the current element == k
-
-        int index = 0;
-
-        while (nums.at(index) <= k){
-            // check if sum of elements left in array == k
-
-            // prep subarray for recursive function
-
-            count += recursiveSum(nums.begin(),nums.begin()+index,k);
-
-            index++;
-        }
-
+        recursiveSum(nums.begin(),nums.end(),k,count);
+        // cout << endl << "RESULT: " << count << endl; 
         return count;
     }
 
-    int recursiveSum(vector<int>::iterator begin,vector<int>::iterator end, int k){
+
+    // a recursive function that takes the sum of current subarray 
+    // and iterates count whenever that sum is equal to k
+    void recursiveSum(vector<int>::iterator begin,vector<int>::iterator end, int& k, int& count){
+        if (begin == end){ // if vector empty, return
+            return;
+        }
+
+        // check sum of current subarray
         int sum = 0;
 
-        while (begin != end){
-            sum += ;
-        }
-    }
+        auto current = begin;
+        // cout << *current << endl;
 
+        while (current != end){
+            sum += *current;
+            ++current;
+            // cout << "rolling sum: " << sum << endl;
+            if (sum == k){
+                count++;
+            }
+        }
+        recursiveSum(++begin, end, k, count); // recursively check subarrays
+    }
 };
 
 int main(){
@@ -44,7 +49,11 @@ int main(){
     Solution s;
 
     vector<int> nums = {1,2,3};
+    vector<int> nums2 = {1,2,1,2,1};
 
+    // cout << endl;
     cout << s.subarraySum(nums,3) << endl; // 2
+
+    cout << s.subarraySum(nums2,3) << endl; // 4
 
 }
